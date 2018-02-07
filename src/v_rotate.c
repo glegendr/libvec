2d45d145e88242d1e813824bdcaaf920d7f26497
Gaspard LEGENDRE <glegendr@e1r9p5.42.fr>
2018-02-07 01:09:12 +0100

    Refactor: pass the norme

--- src/v_rotate.c
+++ src/v_rotate.c
@@ -6,7 +6,7 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 00:04:44 by glegendr          #+#    #+#             */
-/*   Updated: 2018/02/06 00:14:13 by glegendr         ###   ########.fr       */
+/*   Updated: 2018/02/07 01:08:49 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void		v_rotate(t_vec *vec)
 {
 	t_vec v;
-	
+
 	v = v_new(vec->private_elem_size);
 	v_push(&v, v_get(vec, v_size(vec) - 1));
 	v_append_raw(&v, v_get(vec, 0), v_size(vec) - 1);
