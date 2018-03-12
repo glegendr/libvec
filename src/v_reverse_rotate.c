5ff2ab0be3a6280228a2c88fe5a4401a0469985a
Gaspard LEGENDRE <glegendr@e1r4p23.42.fr>
2018-03-12 22:36:26 +0100

    Fix: fix leaks v_reverse

--- src/v_reverse_rotate.c
+++ src/v_reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 00:16:14 by glegendr          #+#    #+#             */
-/*   Updated: 2018/02/15 00:15:17 by glegendr         ###   ########.fr       */
+/*   Updated: 2018/03/12 22:32:27 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ void	v_reverse_rotate(t_vec *vec)
 	ft_memcpy(tmp, v_get(vec, v_size(vec) - 1), vec->private_elem_size);
 	ft_memcpy(tmp2, v_get(vec, 0), (vec->private_elem_nb - 1)
 			* vec->private_elem_size);
+	free(vec->private_content);
+	vec->private_content = malloc(vec->private_elem_cap *
+			vec->private_elem_size);
+	ft_memcpy(v_get(vec, 0), tmp, vec->private_elem_size);
 	ft_memcpy(v_get(vec, 1), tmp2, (vec->private_elem_nb - 1)
 			* vec->private_elem_size);
-	ft_memcpy(v_get(vec, 0), tmp, vec->private_elem_size);
 	free(tmp);
 	free(tmp2);
 }
--- src/v_rotate.c
+++ src/v_rotate.c
@@ -6,7 +6,7 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 00:04:44 by glegendr          #+#    #+#             */
-/*   Updated: 2018/02/15 00:15:28 by glegendr         ###   ########.fr       */
+/*   Updated: 2018/03/12 22:35:00 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,19 @@
 void		v_rotate(t_vec *vec)
 {
 	void *tmp;
+	void *tmp2;
 
+	tmp2 = malloc((vec->private_elem_nb - 1) * vec->private_elem_size);
 	tmp = malloc(vec->private_elem_size);
 	ft_memcpy(tmp, v_get(vec, 0), vec->private_elem_size);
-	ft_memcpy(v_get(vec, 0), v_get(vec, 1), (vec->private_elem_nb - 1)
+	ft_memcpy(tmp2, v_get(vec, 1), vec->private_elem_size *
+			(vec->private_elem_nb - 1));
+	free(vec->private_content);
+	vec->private_content = malloc(vec->private_elem_cap *
+			vec->private_elem_size);
+	ft_memcpy(v_get(vec, 0), tmp2, (vec->private_elem_nb - 1)
 			* vec->private_elem_size);
 	ft_memcpy(v_get(vec, v_size(vec) - 1), tmp, vec->private_elem_size);
+	free(tmp2);
 	free(tmp);
 }
