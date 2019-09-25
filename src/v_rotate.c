d84bff790fd549419102b2a088698d6f8d3c4c2a
glegendr <glegendr@student.42.fr>
2019-09-25 21:25:47 +0200

    Refacto: optimise v_rotate

--- src/v_rotate.c
+++ src/v_rotate.c
@@ -6,7 +6,7 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 00:04:44 by glegendr          #+#    #+#             */
-/*   Updated: 2018/03/12 22:35:00 by glegendr         ###   ########.fr       */
+/*   Updated: 2019/09/25 21:25:14 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,11 @@ void		v_rotate(t_vec *vec)
 	void *tmp;
 	void *tmp2;
 
-	tmp2 = malloc((vec->private_elem_nb - 1) * vec->private_elem_size);
+	tmp2 = v_raw(vec) + vec->private_elem_size;
 	tmp = malloc(vec->private_elem_size);
 	ft_memcpy(tmp, v_get(vec, 0), vec->private_elem_size);
-	ft_memcpy(tmp2, v_get(vec, 1), vec->private_elem_size *
-			(vec->private_elem_nb - 1));
-	free(vec->private_content);
-	vec->private_content = malloc(vec->private_elem_cap *
-			vec->private_elem_size);
-	ft_memcpy(v_get(vec, 0), tmp2, (vec->private_elem_nb - 1)
+	ft_memcpy(v_raw(vec), tmp2, (vec->private_elem_nb - 1)
 			* vec->private_elem_size);
 	ft_memcpy(v_get(vec, v_size(vec) - 1), tmp, vec->private_elem_size);
-	free(tmp2);
 	free(tmp);
 }
