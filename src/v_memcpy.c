2dab5809fd5b779b9a3ac629a212650f3d0baf8c
Gaspard LEGENDRE <glegendr@e1r10p21.42.fr>
2020-03-06 01:36:47 +0100

    Refacto: optimize memcpy

--- src/v_memcpy.c
+++ src/v_memcpy.c
@@ -12,15 +12,40 @@
 
 #include "vector.h"
 
-void	*v_memcpy(void *dst, const void *src, size_t n)
+void			*v_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t i;
+	size_t z;
 
 	i = 0;
-	while (i < n)
+	z = n / 8;
+	while (i < z)
 	{
-		((char*)dst)[i] = ((char *)src)[i];
+		if (((uint64_t *)dst)[i] != ((uint64_t *)src)[i])
+			((uint64_t *)dst)[i] = ((uint64_t *)src)[i];
 		++i;
+	}
+	z = 0;
+	n %= 8;
+	while (z < n)
+	{
+		if (((uint8_t *)dst)[z + 8 * i] != ((uint8_t *)src)[z + 8 * i])
+			((uint8_t *)dst)[z + 8 * i] = ((uint8_t *)src)[z + 8 * i];
+		++z;
 	}
 	return (dst);
 }
+//
+//void	*v_memcpy(void *dst, const void *src, size_t n)
+//{
+//	size_t i;
+//
+//	i = 0;
+//	while (i < n)
+//	{
+//		((char*)dst)[i] = ((char *)src)[i];
+//		++i;
+//	}
+//	return (dst);
+//}
+//
